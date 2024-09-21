9878406c9b2446ffacced1a7afa0641145d4545d
Azhwake <azhwaq2002@gmail.com>
2024-09-21 22:48:14 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Good Luck 💪🏾
+function brokenKeys(correct, typed) {
+    let broken = [];
+    
+    // Loop through the correct phrase
+    for (let i = 0; i < correct.length; i++) {
+        // If the character doesn't match, add to broken list if it's not already there
+        if (correct[i] !== typed[i] && !broken.includes(correct[i])) {
+            broken.push(correct[i]);
+        }
+    }
+    
+    return broken;
+}
+
+// Test cases
+console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp")); // ➞ ["a", "t"]
+console.log(brokenKeys("good night", "gppd night"));               // ➞ ["o"]
